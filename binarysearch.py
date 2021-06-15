e7b62b11d27418083dd4337fc545d3327a41e2af
Anvay Mayekar <74885795+anvaymayekar@users.noreply.github.com>
2021-06-16 02:36:35 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+def binarySearch(array, element):
+    l, u = 0, len(array)
+    if element in array:
+        while l <= u:
+            mid = (l + u)//2
+            if element == array[mid]:
+                return mid
+                break
+            else:
+                if element > array[mid]:
+                    l = mid
+                else:
+                    u = mid           
+    else:
+        raise Exception('Value is not in array')        
+
+b = binarySearch([2,3,5,7,9,12,15], 12)
+print(b)              
