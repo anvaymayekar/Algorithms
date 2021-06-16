0a34ff6d9b0eba03333c0d877bca7c5a5d6bdfeb
Anvay Mayekar <74885795+anvaymayekar@users.noreply.github.com>
2021-06-16 14:33:27 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -12,7 +12,7 @@ def binarySearch(array, element):
                 else:
                     u = mid           
     else:
-        raise Exception('Value is not in array')        
+        raise Exception(f'{element} is not included in the array')       
 
 b = binarySearch([2,3,5,7,9,12,15], 12)
 print(b)              
